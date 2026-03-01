d209588862e5d2fdd8011a919ce3b07f6b34f4ff
Adarsh Shukla <adarshshukla09887@gmail.com>
2026-03-01 21:42:29 +0530

    graph and landing pages

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,32 +1,39 @@
-import { headers } from "next/headers";
-import { auth } from "@/lib/utils/auth";
-import { requireAdmin } from "@/server-actions/admin/auth/route";
-import React from "react";
-import { redirect } from "next/navigation";
-import { todOrder } from "@/server-actions/admin/others/route";
-import LandingPage from "@/components/admin/dashboard/LandingPage";
+import { headers } from "next/headers"
+import { auth } from "@/lib/utils/auth"
+import { requireAdmin } from "@/server-actions/admin/auth/route"
+import React from "react"
+import { redirect } from "next/navigation"
+import { last7DaysStats, todOrder } from "@/server-actions/admin/others/route"
+import LandingPage from "@/components/admin/dashboard/LandingPage"
 
-export default  async function Page() {
-
+export default async function Page() {
   const session = await auth.api.getSession({
-     headers: await headers(),
-   });
-   
-   if (!session) {
-     redirect("/register");
-   }
- await requireAdmin()
-//  await todOrder()
-      
+    headers: await headers(),
+  })
+
+  if (!session) {
+    redirect("/register")
+  }
+
+  await requireAdmin()
+
+  const today = await todOrder()
+ const week = await last7DaysStats();
+  if (!today?.data) {
+    return <div>Error loading dashboard</div>
+  }
+
+  const data = today.data
+const data2 =week.data;
   return (
-    <LandingPage/>
-    // <div className="h-[80vh] flex justify-center items-center">
-    //  <div>
-    //   {/* {session?.user ?    <h1 className="text-3xl font-bold">
-    //     Welcome to the Admin Page
-    //   </h1> : <p>Not logged in</p>
-    //   } */}
-    // </div>
-    // </div>
-  );
+    <LandingPage
+      tableNumber={data.tableNumber}
+      totalStaff={data.totalStaff}
+      TotalOrder={data.TotalOrder}
+      TotalPay={data.TotalPay}
+      TotalMPay={data.TotalMPay}
+      weekRevenue={data2?.revenue}
+      weekOrder={data2?.orders}
+    />
+  )
 }
